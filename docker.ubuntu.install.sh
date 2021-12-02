1276e363d7e3a7ffa318e2cdb7ac87f23a23c4a8
pinguianer <pinguianer@gmail.com>
2021-12-02 12:35:37 +0000

    docker install sh

---
+++ docker.ubuntu.install.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+apt update
+apt install -y apt-transport-https ca-certificates curl software-properties-common
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | apt-key add -
+add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu focal stable"
+apt update
+apt upgrade
+apt update
+apt-cache policy docker-ce
+apt install -y docker-ce
+curl -L "https://github.com/docker/compose/releases/download/1.26.0/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+chmod +x /usr/local/bin/docker-compose
+docker-compose --version
+
